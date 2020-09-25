bdab073dc36f4362cccbdf9abcc1e531c71bcc38
bebenlebricolo <benoittarrade@hotmail.fr>
2020-09-25 11:08:02 +0200

    Added quick PugiXML test
    Signed-off-by: bebenlebricolo <benoittarrade@hotmail.fr>

---
+++ C++/PugiXml/App/app.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+
+#include "pugixml.hpp"
+
+int main (int argc, char** argv)
+{
+    pugi::xml_document doc;
+    pugi::xml_node node;
+    node.set_name("Project");
+
+    return 0;
+}
