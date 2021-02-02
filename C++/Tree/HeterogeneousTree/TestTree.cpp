1fc3c1fc72349869d12ab4cde40ea00c9663a0cb
bebenlebricolo <benoittarrade@hotmail.fr>
2021-02-02 15:09:54 +0100

    Modified Tree architecture to support pseudo Json output
    Signed-off-by: bebenlebricolo <benoittarrade@hotmail.fr>

--- C++/Tree/HeterogeneousTree/TestTree.cpp
+++ C++/Tree/HeterogeneousTree/TestTree.cpp
@@ -1,5 +1,5 @@
 #include "TreeNode.h"
-
+#include <iostream>
 
 int main()
 {
@@ -9,15 +9,24 @@ int main()
         root_node.set_value(child);
     }
 
-    for (uint8_t i = 0U; i < 10U ; i++)
+    for (uint8_t i = 0U; i < 5U ; i++)
     {
         auto new_node = root_node.add_child();
         if (nullptr != new_node)
         {
             new_node->set_name("child node Nb " + std::to_string(i));
-            new_node->set_value("This is my payload : " + std::to_string(i));
+            for (uint j = 0 ;  j < 3U ; j++)
+            {
+                auto insider = new_node->add_child();
+                if (nullptr != insider)
+                {
+                    insider->set_name("Temp property #" + std::to_string(j));
+                    insider->set_value(j*4);
+                }
+            }
         }
     }
 
+    std::cout << root_node.print(2U, 1U).str() << std::endl;
     return 0;
 }
--- C++/Tree/HeterogeneousTree/TreeNode.h
+++ C++/Tree/HeterogeneousTree/TreeNode.h
@@ -10,34 +10,54 @@
 #include <iostream>
 #endif
 
+#include <sstream>
+
+/**
+ * @brief The TreeNode structure provides an implementation for a tree node, encompassing several underlying types such as int, float, std::string, etc.
+ * it also comes with the possibility to store several children nodes.
+*/
 struct TreeNode
 {
+public:
+
+    /**
+     * @brief The Content structure is essentially a tagged union, which allows
+     * to discriminate the active type and handle it correctly
+    */
     struct Content
     {
     public:
+        /**
+         * @brief this enumerate type gives all types supported by this TreeNode
+        */
         enum class Type
         {
-            Unknown,
-            Boolean,
-            String,
-            Float,
-            Double,
-            Int,
-            UInt,
-            List
+            Unknown,    /**< No type specified, equivalent to void *             */
+            Boolean,    /**< Stores a boolean value                              */
+            String,     /**< Stores a std::string                                */
+            Float,      /**< Stores a floating point value                       */
+            Double,     /**< Stores a double precision floating point value      */
+            Int,        /**< Stores an integer value                             */
+            UInt,       /**< Stores an unsigned integer value                    */
+            List        /**< Indicates that this object packs a list of children */
         };
 
+        /**
+         * @brief this union aggregates all types supported by this Content structure, less the list
+         * because of its dynamic behavior (Note, this is the same with std::string type; non trivial types allocation/deallocation)
+        */
         union Payload
         {
             Payload() {}
             ~Payload() {}
 
-            bool Boolean;
-            std::string String;
-            float Float;
-            double Double;
-            int Int;
-            unsigned int UInt;
+            void * Null {nullptr};  /**< Storage for Unknown types  */
+            bool Boolean;           /**< Storage for Boolean types  */
+            float Float;            /**< Storage for floating point */
+            double Double;          /**< Storage for doubles        */
+            int Int;                /**< Storage for integers       */
+            unsigned int UInt;      /**< Storage for unsigned int   */
+            std::string String;     /**< Storage for String types   */
         };
 
         Content() : type(Type::Unknown)
@@ -61,6 +81,7 @@ struct TreeNode
         }
 
     protected:
+        // Give the possibility to TreeNode to manipulate the internals of this Content structure (to reinforce encapsulation and linkage between the 2 types)
         friend TreeNode;
         Type type {Type::Unknown};
         Payload payload;
@@ -68,19 +89,17 @@ struct TreeNode
 
         void initialize_payload();
         void initialize_payload(const Type t);
+        void destroy_payload();
+        void destroy_payload(const Type t);
         void copy_payload(const Content& other);
         void move_payload(Content&& other);
 
-    };
+    }; // End of Content structure
 
     TreeNode() {}
     TreeNode(const std::string& _name) : name(_name)  {}
     TreeNode(const TreeNode& other) : name(other.name), content(other.content) {}
-    ~TreeNode()
-    {
-        // Clears payload and recursively calls destructors for everybody
-        content.initialize_payload();
-    }
+    ~TreeNode();
 
     template<typename T>
     TreeNode(const std::string& _name, const T& value) : name(_name)
@@ -93,31 +112,34 @@ struct TreeNode
 
     void set_name(const std::string& _name) {name = _name;}
     std::string get_name() const {return name;}
+    std::ostringstream print(const uint indent = 4, const uint level = 1) const;
+
+    template<typename T>
+    T get();
+
     std::vector<std::shared_ptr<TreeNode>>* get_list();
     std::shared_ptr<TreeNode> add_child();
+    std::shared_ptr<TreeNode> get_parent();
 
     template<typename T>
     void set_value(const T& value);
-    //{
-    //    (void) value;
-    //}
 
     template<typename T>
     void set_value(T& value);
-    //{
-    //    (void) value;
-    //}
 
     template<typename T>
     void set_value(T&& value);
-    //{
-    //    (void) value;
-    //}
 
 protected:
+    std::shared_ptr<TreeNode> parent_node {nullptr};    /**< This field stores a pointer to a potential parent node*/
     std::string name{"Default"};
     Content content;
 };
+
+
+/*  ##################################################################################
+    ############################## Implementation ####################################
+    ################################################################################## */
 
 template<>
 void TreeNode::set_value<bool>(const bool& value)
@@ -184,10 +206,57 @@ void TreeNode::set_value<TreeNode>(TreeNode&& value)
     }
 }
 
+template<>
+void TreeNode::set_value<unsigned int>(unsigned int&& value)
+{
+    content.type = Content::Type::UInt;
+    content.payload.UInt = std::move(value);
+}
+
+template<>
+void TreeNode::set_value<int>(int&& value)
+{
+    content.type = Content::Type::Int;
+    content.payload.Int = std::move(value);
+}
+
+template<>
+void TreeNode::set_value<float>(float&& value)
+{
+    content.type = Content::Type::Float;
+    content.payload.Float = std::move(value);
+}
+
+template<>
+void TreeNode::set_value<double>(double&& value)
+{
+    content.type = Content::Type::Double;
+    content.payload.Double = std::move(value);
+}
+
+TreeNode::~TreeNode()
+{
+    // Clears payload and recursively calls destructors for everybody
+    content.destroy_payload();
+}
 
 void TreeNode::Content::initialize_payload()
 {
     initialize_payload(type);
+}
+
+void TreeNode::Content::destroy_payload()
+{
+    destroy_payload(type);
+}
+
+void TreeNode::Content::destroy_payload(const Type t)
+{
+    if (t == Type::String)
+    {
+        using std::string;
+        payload.String.~string();
+    }
 }
 
 void TreeNode::Content::initialize_payload(const Type t)
@@ -215,7 +284,7 @@ void TreeNode::Content::initialize_payload(const Type t)
             break;
 
         case Type::String:
-            payload.String = "";
+            new(&payload.String) std::string();
             break;
 
         case Type::List:
@@ -301,6 +370,174 @@ void TreeNode::Content::move_payload(Content&& other)
     }
 }
 
+std::string surround_with_quotes(const std::string& input)
+{
+    std::string out = "\"" + input + "\"";
+    return out;
+}
+
+std::string generate_indent(const uint indent)
+{
+    std::string out;
+    for (uint i = 0 ;  i < indent ; i++)
+    {
+        out += ' ';
+    }
+    return out;
+}
+
+std::ostringstream TreeNode::print(const uint indent, const uint level) const
+{
+    std::ostringstream out;
+    std::string indent_str = generate_indent(indent * level);
+    out << "{" << std::endl
+        << indent_str << surround_with_quotes("name") << " : " << surround_with_quotes(name) << "," << std::endl;
+
+    switch(content.type)
+    {
+        case Content::Type::Boolean:
+            out << indent_str << surround_with_quotes("bool") << " : " << surround_with_quotes(((content.payload.Boolean == true) ? "true" : "false")) << std::endl;
+            break;
+
+        case Content::Type::Float:
+            out << indent_str << surround_with_quotes("float") << " : " << surround_with_quotes(std::to_string(content.payload.Float)) << std::endl;
+            break;
+
+        case Content::Type::UInt:
+            out << indent_str << surround_with_quotes("uint") << " : " << surround_with_quotes(std::to_string(content.payload.UInt)) << std::endl;
+            break;
+
+        case Content::Type::Int:
+            out << indent_str << surround_with_quotes("int") << " : " << surround_with_quotes(std::to_string(content.payload.Int)) << std::endl;
+            break;
+
+        case Content::Type::Double:
+            out << indent_str << surround_with_quotes("double") << " : " << surround_with_quotes(std::to_string(content.payload.Double)) << std::endl;
+            break;
+
+        case Content::Type::String:
+            out << indent_str << surround_with_quotes("string") << " : " << surround_with_quotes(content.payload.String) << std::endl;
+            break;
+
+        case Content::Type::List:
+            {
+                std::string indent_str = generate_indent(indent * level);
+                out << indent_str << surround_with_quotes("children") << " : " << "[" << std::endl;
+
+
+                for (const auto& ptr : content.children)
+                {
+                    // Start object node
+                    indent_str = generate_indent(indent * (level + 1));
+                    out << indent_str << ptr->print(indent, level + 2).str();
+
+                    // Only add a coma for items in-between children's boundaries.
+                    // Skip the last element of the vector
+                    if (&ptr != &content.children.back())
+                    {
+                        out << "," << std::endl;
+                    }
+                    //out << std::endl << indent_str << "}" << std::endl;
+                }
+                out << std::endl << generate_indent(indent * level) << "]" << std::endl;
+            }
+            break;
+
+        case Content::Type::Unknown:
+        default:
+            out << surround_with_quotes("Null");
+            break;
+    }
+
+    if (content.type != Content::Type::Unknown)
+    {
+        indent_str = generate_indent(indent * (level - 1));
+        out << indent_str << "}";
+    }
+    return out;
+}
+
+
+template<>
+bool TreeNode::get()
+{
+    if (content.type == Content::Type::Boolean)
+    {
+        return content.payload.Boolean;
+    }
+    return false;
+}
+
+template<>
+double TreeNode::get()
+{
+    if (content.type == Content::Type::Double)
+    {
+        return content.payload.Double;
+    }
+    return 0.0f;
+}
+
+template<>
+float TreeNode::get()
+{
+    if (content.type == Content::Type::Float)
+    {
+        return content.payload.Float;
+    }
+    return 0.0f;
+}
+
+template<>
+unsigned int TreeNode::get()
+{
+    if (content.type == Content::Type::UInt)
+    {
+        return content.payload.UInt;
+    }
+    return 0U;
+}
+
+template<>
+int TreeNode::get()
+{
+    if (content.type == Content::Type::Int)
+    {
+        return content.payload.Int;
+    }
+    return 0U;
+}
+
+template<>
+std::vector<std::shared_ptr<TreeNode>> TreeNode::get()
+{
+    if (content.type == Content::Type::List)
+    {
+        return *get_list();
+    }
+    return std::vector<std::shared_ptr<TreeNode>>();
+}
+
+template<>
+std::string TreeNode::get()
+{
+    if (content.type == Content::Type::String)
+    {
+        return content.payload.String;
+    }
+    return std::string();
+}
+
+template<>
+void * TreeNode::get()
+{
+    if (content.type == Content::Type::Unknown)
+    {
+        return content.payload.Null;
+    }
+    return nullptr;
+}
+
 std::vector<std::shared_ptr<TreeNode>>* TreeNode::get_list()
 {
     std::vector<std::shared_ptr<TreeNode>>* out = nullptr;
@@ -314,20 +551,25 @@ std::vector<std::shared_ptr<TreeNode>>* TreeNode::get_list()
 std::shared_ptr<TreeNode> TreeNode::add_child()
 {
     std::shared_ptr<TreeNode>* out = nullptr;
-    if (content.type == Content::Type::List)
+    if (content.type != Content::Type::List)
     {
-        std::shared_ptr<TreeNode> pointer = std::make_shared<TreeNode>();
-        if (pointer != nullptr)
-        {
-            content.children.push_back(pointer);
-            out = &pointer;
-        }
+        content.destroy_payload();
+        content.type = Content::Type::List;
+        content.initialize_payload();
+    }
+
+    std::shared_ptr<TreeNode> pointer = std::make_shared<TreeNode>();
+    if (pointer != nullptr)
+    {
+        content.children.push_back(pointer);
+        out = &pointer;
     }
 
     if (nullptr != out)
     {
         return *out;
     }
+
 
     return std::shared_ptr<TreeNode>(nullptr);
 }
