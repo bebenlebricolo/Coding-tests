28f733aaa23e2f91e2f3dbb5313a1c7bc1e98a15
bebenlebricolo <benoittarrade@hotmail.fr>
2022-03-14 15:59:59 +0100

    Pending C++ tests
    Signed-off-by: bebenlebricolo <benoittarrade@hotmail.fr>

--- C++/garbage_struct_init/garbage_struct_init.cpp
+++ C++/garbage_struct_init/garbage_struct_init.cpp
@@ -1,11 +1,18 @@
 #include <stdlib.h>
 #include <cstdint>
-#include <cassert>
 #include <iostream>
 #include <string>
 
 struct Test
 {
+    inline void print()
+    {
+        std::cout << "double b : "<< std::to_string(b) << std::endl
+                  << "int a : " << std::to_string(a) << std::endl
+                  << "char c : " << std::to_string(c) << std::endl
+                  << "unsigned int d : " << std::to_string(d) << std::endl << std::endl;
+    }
+
     double b;
     int a;
     char c;
@@ -15,8 +22,12 @@ struct Test
 int main()
 {
     constexpr uint8_t testsize = sizeof(Test);
+    std::cout << "test size is : " << std::to_string(testsize) << " bytes wide " << std::endl;
     Test test;
+    test.print();
+
     uint8_t* start = reinterpret_cast<uint8_t*>(&test);
+    std::cout << "Printing test structure byte by byte : " << std::endl;
     for (uint8_t i = 0 ; i < testsize ; i++)
     {
         char buffer[5] = {0};
